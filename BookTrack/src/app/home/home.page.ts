26c82894d11600b053774904c7a65d0ede478821
Crantly1 <77400072+Crantly1@users.noreply.github.com>
2021-02-03 21:49:23 -0500

    added features from feature-add-book branch to main

--- BookTrack/src/app/home/home.page.ts
+++ BookTrack/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -19,7 +20,53 @@ export class HomePage implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'New Book',
+      subHeader: 'Add a book to your list.',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          id: 'title-id',
+          placeholder: 'title'
+        },
+        {
+          name: 'author',
+          type: 'text',
+          id: 'author-id',
+          placeholder: 'author'
+        }],
+
+      buttons: [
+        {
+          text: 'Submit',
+          role: 'submit',
+          handler: data => {
+            this.addBook(data.title, data.author)
+          }
+        }, 
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+
+    });
+
+    await alert.present();
+  }
+  
+  //called when "submit" is clicked to add a new book
+  addBook(newTitle: string, newAuthor: string){
+    console.log(newTitle + " " + newAuthor);
+    this.books.push({name: newTitle, author: newAuthor})
+  }
 
   ngOnInit() {
   }
